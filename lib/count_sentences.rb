502e906bfec0d6cd6d6471433381fed790a058bb
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-09 02:56:01 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,29 @@
 require 'pry'
 
 class String
-
+  
+  attr_accessor = :sentence
+  attr_reader = :sentence
+  
+  def intialize(sentence)
+    @sentence = sentence
+  end 
+  
   def sentence?
-    
+    self.split("")[-1] == "."  
   end
-
+  
   def question?
-
+    self.split("")[-1] == "?"    
   end
 
   def exclamation?
-
+    self.split("")[-1] == "!"  
   end
-
+    
   def count_sentences
+    delimiters = ['.', "!"]
 
+    self.split(Regexp.union(delimiters)).count
   end
 end
